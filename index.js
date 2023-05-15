2e4cbd545cb1098358a91c8c042258d5c2fe3058
Jonarver Molina <jmolina@xxxx.com>
2023-05-14 22:59:47 -0400

    resto de archivos

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 const path = require('path');
 
 const express = require('express');
-
+const cors = require('cors');
 
 const { dbConnection } = require('./database/config');
 
 // Crear el servidor de express
 const app = express();
+
+// Configurar CORS
+app.use(cors());
 
 // Lectura y parseo del body
 app.use(express.json());
